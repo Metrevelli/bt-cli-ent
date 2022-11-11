754f9aef57cf821630437d35bb9ef60ac19da5d5
Nika <niko.metreveli@gmail.com>
2022-11-11 17:54:49 +0400

    add onWholeMsg function

--- src/download.js
+++ src/download.js
@@ -14,5 +14,27 @@ function download(peer) {
   const socket = new net.Socket();
   socket.on('error', console.log);
   socket.connect(peer.port, peer.ip, () => {});
+  onWholeMsg(socket, (data) => {});
   socket.on('data', (data) => {});
+}
+
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+  // handshake message is allways recieved first and its only way to tell
+  // if we are recieving handshake... it also doesnt tell its length
+  // as part of the message
+  let handshake = true;
+
+  socket.on('data', (recvBuf) => {
+    const msgLen = () =>
+      // 68 is length of handshake buffer
+      handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
+  });
 }
